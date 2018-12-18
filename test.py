68f20e2a5a0898a94690269a4310da805de7cd39
sweng <shiqiang-tommy.weng@nokia-sbell.com>
2018-12-18 14:35:21 +0800

    Modify test.py

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ def choose_cores_for_tti_trace_gathering_for_both_directions(cell_number=1, is_n
         #tti_trace_core_num = self.ta_dev_wro1.get_cpu_address_for_tti_traces(cell, "both", is_nb_iot)
         #result[cell] = tti_trace_core_num
 
+
+
     return result
 
 if __name__ == '__main__':
